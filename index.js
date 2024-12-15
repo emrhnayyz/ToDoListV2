4110aa61a80f053d1edba1131c005903a9942498
emrhnayzz <emirhan.ayaz171@gmail.com>
2024-12-16 00:19:41 +0100

    delete funktion added

--- index.js
+++ index.js
@@ -19,16 +19,25 @@ try {
     const result = await db.query("SELECT * FROM tasks ORDER BY task_id ASC");
     res.render("index.ejs",{
         tasks:result.rows
-    });
-
-    
-
-    
+    });    
 } catch (error) {
     console.log(error);
     res.status(500).send("Server Error");
     
 }
+})
+
+app.post("/delete/:id", async(req,res)=>{
+    const taskId = req.params.id
+
+    try {
+        await db.query("DELETE FROM tasks WHERE task_id = $1", {taskId});
+        res.redirect("/")
+    } catch (error) {
+        console.log("Görev Silme Hatasi", error);
+        res.status(500).send("Sunucu Hatasi: Görev silinemedi.");
+        
+    }
 })
 
 app.use(bodyParser.urlencoded({extended:true}));
